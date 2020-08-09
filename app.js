3c0da46e346c74600d3fdf73a494184ddc28e0e8
Kev ALDASORO <kev-andrew.aldasoro@thalesgroup.com>
2020-08-09 15:14:20 -0500

    Add bitmoji image encrypted

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
 var indexRouter = require('./routes/index')
+var bitmojiRouter = require('./routes/bitmoji')
 
 var app = express()
 
@@ -19,6 +20,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
+
+app.use('/bitmoji', bitmojiRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/bitmoji.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var router = express.Router()
+var request = require('request')
+
+const BITMOJI = [
+  '9e669e76-bd42-43ba-bc81-83741de280f5',
+  'a41da708-81e9-4ec3-9364-9d8748f7063d',
+  '8f42bf78-97e6-4e35-8ec5-709ed099f8e9',
+  '096dffe0-3934-41db-842c-34c180d0615c',
+  '36d2d62a-fc90-4604-933b-52a066b5c1b4',
+  'fc5507ba-0b77-439b-a135-1fa8827aa188',
+  '76c3b171-f0cf-4dd6-b91f-91bd86693f61'
+]
+
+/* GET health. */
+router.get('/', function (_, res, next) {
+  const bitmojiUrl = `https://sdk.bitmoji.com/render/panel/${BITMOJI[Math.floor(Math.random() * BITMOJI.length)]}-36692507-a781-4645-83d1-17df60d447bc-v1.png?transparent=1&palette=1`
+  var image
+  request({
+    url: bitmojiUrl,
+    method: 'GET',
+    encoding: null // This is actually important, or the image string will be encoded to the default encoding
+  }, (error, _, body) => {
+    if (error) next(error)
+
+    image = body.toString('base64')
+    res.status(200).send(`data:image/png;base64,${image}`)
+  })
+})
+
+module.exports = router
