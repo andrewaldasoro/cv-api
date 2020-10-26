554fd90569b53dcb158d785dcdf3a6d036effd56
Kev <andrewaldasoro@gmail.com>
2020-10-25 23:03:59 -0400

    Allow subdomain cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors')
 var express = require('express')
+var cors = require('cors')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
@@ -16,6 +17,11 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
 app.use(logger('dev'))
+app.use(cors({
+  origin: /\.andrewaldasoro\.me$/,
+  methods: ['GET', 'POST'],
+  optionsSuccessStatus: 200
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
