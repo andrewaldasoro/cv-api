43c1d34434658394bb4cd6e75a1fc732d21fe311
Kev <andrewaldasoro@gmail.com>
2020-10-26 00:05:34 -0400

    Add version to a request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var router = express.Router()
+const pjson = require('../package.json')
 
 /* GET health. */
 router.get('/', function (req, res, next) {
   res.status(200).send('')
+})
+
+router.get('/', function (req, res, next) {
+  res.status(200).send(pjson.version)
 })
 
 module.exports = router
